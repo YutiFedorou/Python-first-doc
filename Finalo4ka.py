3eecaca355439164e262a5d7d1d6f90cef52496e
Yuriy Fedorov <yuraf@list.ru>
2021-09-08 23:45:00 +0300

    (Banks)

--- Finalo4ka.py
+++ Finalo4ka.py
@@ -3,11 +3,11 @@
 money = int(input("Введите сумму:"))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 
-TKB = int((per_cent['ТКБ']) * (money/100))
-SKB = int((per_cent['СКБ']) * (money/100))
-VTB = int((per_cent['ВТБ']) * (money/100))
-SBER = int((per_cent['СБЕР']) * (money/100))
-Deposit = [TKB, SKB, VTB, SBER]
+Bank1 = int((per_cent['ТКБ']) * (money/100))
+Bank2 = int((per_cent['СКБ']) * (money/100))
+Bank3 = int((per_cent['ВТБ']) * (money/100))
+Bank4 = int((per_cent['СБЕР']) * (money/100))
+Deposit = [Bank1, Bank2, Bank3, Bank4]
 print('Все банковские ставки:',(Deposit))
 print('Самая выгодная ставка:',(max(Deposit)))
 print(per_cent.values())
